5ce573e09625d54d25d410c3079cb31b277b3e67
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-09-20 17:10:25 -0500

    refactor: update image list details view

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,18 +1,22 @@
 <template>
-    <div class="image-gallery-field">
-        <ul
-            v-if="field.value && field.value.length"
-            class="flex flex-wrap image-list"
-        >
-            <FieldImage
-                v-for="image in field.value"
-                :image="image"
-                :key="image.id"
-                readonly
-            />
-        </ul>
-        <p v-else>-</p>
-    </div>
+    <PanelItem :index="index" :field="field">
+        <template #value>
+            <div class="image-gallery-field">
+                <ul
+                    v-if="field.value && field.value.length"
+                    class="flex flex-wrap image-list -mt-4"
+                >
+                    <FieldImage
+                        v-for="image in field.value"
+                        :image="image"
+                        :key="image.id"
+                        readonly
+                    />
+                </ul>
+                <p v-else>-</p>
+            </div>
+        </template>
+    </PanelItem>
 </template>
 
 <script>
