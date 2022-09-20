cbf35cea486cc3a339ccfe0e44a177b3562e3778
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-09-20 17:42:02 -0500

    feat: add test coverage for the store pending image job

---
+++ tests/StorePendingImageTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Nova\Trix\PendingAttachment;
+use Illuminate\Validation\ValidationException;
+use Ardenthq\ImageGalleryField\ImageGalleryField;
+use Ardenthq\ImageGalleryField\StorePendingImage;
+
+function createRequestWithFile(UploadedFile $file, array $parameters = [])
+{
+    return new class($file, $parameters) extends Request {
+        public function __construct(UploadedFile $file, array $parameters)
+        {
+            parent::__construct(files: ['attachment' => $file], query: $parameters);
+        }
+    };
+}
+
+
+it('creates an instance', function () {
+    $field = new ImageGalleryField('content');
+
+    $job = new StorePendingImage($field);
+
+    expect($job->field)->toBe($field);
+});
+
+it('requires an attachment', function () {
+    $field = new ImageGalleryField('content');
+
+    $job = new StorePendingImage($field);
+
+    $job->__invoke(request()->merge([
+        'draftId' => '123',
+    ]));
+})->throws(ValidationException::class, 'The attachment field is required.');
+
+it('requires a draftId', function () {
+    $field = new ImageGalleryField('content');
+
+    $job = new StorePendingImage($field);
+
+    $job->__invoke(request()->merge([
+        'attachment' => UploadedFile::fake()->image('image.jpg'),
+    ]));
+})->throws(ValidationException::class, 'The draft id field is required.');
+
+it('stores the attachment and returns a json with the url and model id', function () {
+    Storage::fake('public');
+
+    $field = new ImageGalleryField('content');
+
+    $job = new StorePendingImage($field);
+
+    $image = UploadedFile::fake()->image('image.png');
+
+    $request = createRequestWithFile($image, [
+        'draftId' => '123',
+    ]);
+
+    $response = $job->__invoke($request);
+
+    expect(PendingAttachment::count())->toBe(1);
+
+    $attachment = PendingAttachment::first();
+
+    expect($attachment->draft_id)->toBe('123');
+    expect($attachment->disk)->toBe('public');
+    expect($attachment->attachment)->toEndWith('.png');
+
+    expect($response)->toBeJson();
+
+    expect($response)->toContain(sprintf('{"url":"\/storage\/%s","id":%s}', $attachment->attachment, $attachment->id));
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,53 @@ declare(strict_types=1);
 namespace Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
+use Illuminate\Database\Schema\Blueprint;
 
 abstract class TestCase extends Orchestra
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setUpDatabase($this->app);
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     */
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'sqlite');
+        config()->set('database.connections.sqlite', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     */
+    protected function setUpDatabase($app)
+    {
+        $app['db']->connection()->getSchemaBuilder()->create('nova_pending_trix_attachments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('draft_id')->index();
+            $table->string('attachment');
+            $table->string('disk');
+            $table->timestamps();
+        });
+
+        $app['db']->connection()->getSchemaBuilder()->create('nova_trix_attachments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('attachable_type');
+            $table->unsignedInteger('attachable_id');
+            $table->string('attachment');
+            $table->string('disk');
+            $table->string('url')->index();
+            $table->timestamps();
+
+            $table->index(['attachable_type', 'attachable_id']);
+        });
+    }
 }
